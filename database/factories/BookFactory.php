c4bf79b8cb05f433452586a6fa1046fe8c436296
krisnaadi <man.krisna@gmail.com>
2024-01-28 14:01:38 +0000

    init

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Author;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentences(1, true),
+            'category_id' => Category::factory()->create()->id,
+            'year' => date('Y'),
+            'author_id' => Author::factory()->create()->id,
+        ];
+    }
+}
